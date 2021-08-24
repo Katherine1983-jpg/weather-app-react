f013132365b805d26c0aeb2ff3a6447293eab366
Katherine1983-jpg <79195808+Katherine1983-jpg@users.noreply.github.com>
2021-08-24 19:27:03 +0700

    Added npm bootstrap

--- src/App.js
+++ src/App.js
@@ -3,25 +3,27 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project is coded by{" "}
-        <a
-          href="https://katherine-simpson.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Katherine Simpson
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/Katherine1983-jpg/weather-app-react"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced.
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project is coded by{" "}
+          <a
+            href="https://katherine-simpson.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Katherine Simpson
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/Katherine1983-jpg/weather-app-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced.
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
