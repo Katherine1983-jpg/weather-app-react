f2a60c99c107fea74de3ffccf456ad06b493aafc
Katherine1983-jpg <79195808+Katherine1983-jpg@users.noreply.github.com>
2021-08-24 18:46:18 +0700

    Added footer

--- src/App.js
+++ src/App.js
@@ -4,6 +4,16 @@ export default function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
+      <footer>
+        This project is coded by{" "}
+        <a href="" target="_blank" rel="noreferrer">
+          Katherine Simpson
+        </a>{" "}
+        and is{" "}
+        <a href="" target="_blank" rel="noreferrer">
+          open-sourced.
+        </a>
+      </footer>
     </div>
   );
 }
